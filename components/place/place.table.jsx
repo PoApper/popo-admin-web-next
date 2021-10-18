7c1156775c958c0ebc21a831bbb7fe0c218ee691
bluehorn07 <bluehornblues@gmail.com>
2021-10-19 01:16:15 +0900

    implt plate list page

---
+++ components/place/place.table.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import { Table } from 'semantic-ui-react'
+import moment from 'moment'
+import axios from 'axios'
+
+const regionNames = {
+  'STUDENT_HALL': '학생 회관',
+  'JIGOK_CENTER': '지곡 회관',
+  'OTHERS': 'OTHERS',
+}
+const PlaceTable = () => {
+  const [places, setPlaces] = useState([])
+
+  useEffect(async () => {
+    try {
+      const res = await axios.get(`${process.env.NEXT_PUBLIC_API}/place`)
+      setPlaces(res.data)
+    } catch (err) {
+      alert('장소 목록을 불러오는데 실패했습니다.')
+      console.log(err)
+    }
+  })
+
+  return (
+    <Table
+      celled selectable
+      textAlign={'center'} color={'orange'}>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>idx.</Table.HeaderCell>
+          <Table.HeaderCell>장소명</Table.HeaderCell>
+          <Table.HeaderCell>위치</Table.HeaderCell>
+          <Table.HeaderCell>지역</Table.HeaderCell>
+          <Table.HeaderCell>생성일</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {
+          places.map((place, idx) =>
+            <Table.Row key={idx}>
+              <Table.Cell>{idx + 1}</Table.Cell>
+              <Table.Cell>{place.name}</Table.Cell>
+              <Table.Cell>{place.location}</Table.Cell>
+              <Table.Cell>{regionNames[place.region]}</Table.Cell>
+              <Table.Cell>
+                {moment(place.createdAt).format('YYYY년 MM월 DD일 HH:mm')}
+              </Table.Cell>
+            </Table.Row>)
+        }
+      </Table.Body>
+      <Table.Footer>
+        <Table.Row>
+        </Table.Row>
+      </Table.Footer>
+    </Table>
+  )
+}
+
+export default PlaceTable
---
+++ pages/reservation/placeList.jsx
@@ -0,0 +1,12 @@
+import PlaceTable from '../../components/place/place.table'
+import ReservationLayout from '../../components/reservation/reservation.layout'
+
+const PlaceListPage = () => {
+  return (
+    <ReservationLayout>
+      <PlaceTable/>
+    </ReservationLayout>
+  )
+}
+
+export default PlaceListPage
