e74faf33ff8c45bba7680f25b4b9e584f56c882c
bluehorn07 <bluehornblues@gmail.com>
2021-11-08 23:08:11 +0900

    update index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,40 @@
 import Layout from '../components/layout'
-import { Image } from 'semantic-ui-react'
+import { Divider, Grid, Image, List } from 'semantic-ui-react'
 
 const HomePage = () => {
   return (
     <Layout>
-      <h1>안녕하세요, POPO 관리자 페이지 입니다🙌</h1>
-      <p>
-        이곳에서 POPO 사이트의 기능과 데이터베이스를 관리할 수 있습니다.
-        새로운 장비와 장소를 추가해 대여할 수도 있죠.
-        POPO를 가꾸는 것은 여러분 손에 달려 있습니다.
-      </p>
-      <Image
-        centered
-        src={'/home_background.jpg'}
-        alt={'home_background'}
-        size={'big'}/>
+      <Grid columns="equal">
+        <Grid.Column>
+          <h2>POPO 관리자 페이지🎩</h2>
+          <p style={{fontSize: "16px", marginBottom: "2rem"}}>
+            안녕하세요, POPO의 관리자 페이지입니다.
+            이곳에서 POPO 사이트의 기능과 데이터베이스를 관리할 수 있습니다.
+            POPO 서비스가 지속되기 위해선 관리자 여러분의 노력이 필요합니다. 🙏
+            <br/><br/>
+            현재 POPO의 유지/보수는 동아리 PoApper가 진행하고 있습니다. 서비스 장애시 PoApper에 문의 부탁드립니다 👨‍💻
+          </p>
+          <Divider/>
+          <h2>ToDo List 🚀</h2>
+          <List as='ul'>
+            <List.Item as='li'>게시판 기능</List.Item>
+            <List.Item as='li'>PoApper SSO 유저 정보 이전</List.Item>
+            <List.Item as='li'>생활백서 데이터베이스화</List.Item>
+            <List.Item as='li'>Full Responsive Web</List.Item>
+          </List>
+        </Grid.Column>
+        <Grid.Column>
+          <div>
+            <Image
+              centered rounded
+              size={'medium'}
+              src={'/home_background.jpg'}
+              alt={'home_background'}
+            />
+          </div>
+        </Grid.Column>
+      </Grid>
+
     </Layout>
   )
 }
