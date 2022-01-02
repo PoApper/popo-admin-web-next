2751757c4e63bdbb2a28e033aad50fda8a91b58d
bluehorn07 <bluehornblues@gmail.com>
2022-01-03 01:21:14 +0900

    implt 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import { Button, Image } from 'semantic-ui-react'
+import LoginLayout from '../components/layout.raw'
+
+const ErrorPage = () => {
+  return (
+    <LoginLayout>
+      <Image
+        src={'/popo.svg'}
+      />
+      <h2>
+        잘못된 주소이거나 현재 개발중인 페이지 입니다 🙏
+      </h2>
+      <Link href={'/'} passHref>
+        <Button>
+          홈으로 이동
+        </Button>
+      </Link>
+    </LoginLayout>
+  )
+}
+
+export default ErrorPage
