5eee73360d2093ac760f897f58ef3cde57ed2501
jjeongone <jjeongone@gmail.com>
2022-03-24 10:30:33 +0900

    [fix] add community center to place table

--- components/place/place.table.jsx
+++ components/place/place.table.jsx
@@ -7,6 +7,7 @@ import PlaceUpdateModal from './place.update.modal'
 const regionNames = {
   'STUDENT_HALL': '학생 회관',
   'JIGOK_CENTER': '지곡 회관',
+  'COMMUNITY_CENTER': '커뮤니티 센터',
   'OTHERS': 'OTHERS',
 }
 const PlaceTable = () => {
