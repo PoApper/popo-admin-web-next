f68ede1520b328638ad846bdcac201a9b791b7d6
jjeongone <jjeongone@gmail.com>
2022-03-28 19:58:37 +0900

    [fix] add community center to place table

--- components/place/place.update.modal.jsx
+++ components/place/place.update.modal.jsx
@@ -46,6 +46,7 @@ const PlaceUpdateModal = (props) => {
   const regionOptions = [
     { key: 'STUDENT_HALL', text: '학생 회관', value: 'STUDENT_HALL' },
     { key: 'JIGOK_CENTER', text: '지곡 회관', value: 'JIGOK_CENTER' },
+    { key: 'COMMUNITY_CENTER', text: '커뮤니티 센터', value: 'COMMUNITY_CENTER' },
     { key: 'OTHERS', text: '생활관 외', value: 'OTHERS' },
   ]
 
