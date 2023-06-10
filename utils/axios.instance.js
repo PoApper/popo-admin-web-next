2ad51c90137dfa65fe0e8dd86aa0fb59f1805463
bluehorn07 <bluehornblues@gmail.com>
2023-06-10 22:06:09 +0900

    url determin logic

--- utils/axios.instance.js
+++ utils/axios.instance.js
@@ -2,16 +2,10 @@ import axios from "axios";
 
 const next_env = process.env.NEXT_PUBLIC_ENV;
 
-export let popoApiUrl;
-
-switch (next_env) {
-  case "prod":
-    popoApiUrl = "https://api.popo.poapper.club";
-    break;
-  case "dev":
-    popoApiUrl = "https://api.dev.popo.poapper.club";
-    break;
-}
+export let popoApiUrl =
+  next_env === "prod"
+    ? "https://api.popo.poapper.club"
+    : "https://api.dev.popo.poapper.club";
 
 export const PoPoAxios = axios.create({
   baseURL: popoApiUrl,
