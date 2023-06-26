351ae69a71182c1941aef8b4e1645ae7e6a49074
bluehorn07 <bluehornblues@gmail.com>
2023-06-26 23:43:54 +0900

    add image-upload util

---
+++ utils/image-upload.js
@@ -0,0 +1,11 @@
+import { PoPoAxios } from "./axios.instance";
+
+export function ImageUpload(uri, image_file) {
+  let formData = new FormData();
+  formData.append("image", image_file);
+
+  return PoPoAxios.post(uri, formData, {
+    withCredentials: true,
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+}
