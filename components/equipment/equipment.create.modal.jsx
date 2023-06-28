d7af75e801219738f3ef6a3698c148bc0ffb626e
bluehorn07 <bluehornblues@gmail.com>
2023-06-28 13:31:29 +0900

    edit equip create modal w/o image upload

--- components/equipment/equipment.create.modal.jsx
+++ components/equipment/equipment.create.modal.jsx
@@ -1,4 +1,4 @@
-import { Form, Modal } from 'semantic-ui-react'
+import { Form, Message, Modal } from 'semantic-ui-react'
 import { useState } from 'react'
 import { OwnerOptions } from '../../assets/owner.options'
 import { PoPoAxios } from "../../utils/axios.instance";
@@ -12,34 +12,30 @@ const EquipmentCreateModal = (props) => {
   const [description, setDescription] = useState('')
   const [staff_email, setStaffEmail] = useState('')
   const [max_minutes, setMaxMinutes] = useState()
-  const [image, setImage] = useState()
 
   const handleSubmit = async () => {
-    try {
-      let formData = new FormData()
-      formData.append('name', name)
-      formData.append('equip_owner', equip_owner)
-      formData.append('fee', fee)
-      formData.append('description', description)
-      formData.append('staff_email', staff_email)
-      if (max_minutes) {
-        formData.append('max_minutes', max_minutes)
-      }
-      if (image) {
-        formData.append('image', image)
-      }
-      await PoPoAxios.post('/equip', formData,
-        {
-          withCredentials: true,
-          headers: { 'Content-Type': 'multipart/form-data' },
-        },
-      )
-      setOpen(false)
-      window.location.reload()
-    } catch (e) {
-      alert('장비 생성에 실패했습니다.')
-      console.log(e)
+    const body = {
+      'name': name,
+      'equip_owner': equip_owner,
+      'fee': fee,
+      'description': description,
+      'staff_email': staff_email,
+    };
+
+    if (max_minutes) {
+      body['max_minutes'] = max_minutes;
     }
+
+    PoPoAxios.post('/equip', 
+      body,
+      { withCredentials: true },
+    ).then(() => {
+      setOpen(false);
+      window.location.reload();
+    }).catch((err) => {
+      alert('장비 생성에 실패했습니다.');
+      console.log(e);
+    });
   }
 
   return (
@@ -66,24 +62,28 @@ const EquipmentCreateModal = (props) => {
               onChange={(e, { value }) => setEquipOwner(value)}
             />
           </Form.Group>
+
           <Form.Input
             required
             label={'예약 비용'}
             value={fee}
             onChange={e => setFee(e.target.value)}
           />
+
           <Form.Input
               label={'최대 예약가능 시간'}
               placeholder={'해당 장비를 예약가능한 최대 시간을 분단위로 입력해주세요 (ex. 60)'}
               onChange={e => setMaxMinutes(e.target.value)}
           />
           <p>최대 예약가능 시간이 넘는 예약이 생성되지 않도록 합니다.</p>
+
           <Form.TextArea
             required
             label={'설명'}
             value={description}
             onChange={e => setDescription(e.target.value)}
           />
+
           <Form.Input
             label={'담당자 이메일'}
             placeholder="장비 예약을 처리할 담당자의 이메일을 작성해주세요"
@@ -91,12 +91,15 @@ const EquipmentCreateModal = (props) => {
             onChange={e => setStaffEmail(e.target.value)}
           />
           <p>장비 예약이 생성되면, 담당자 메일로 예약 생성 메일이 갑니다.</p>
-          <Form.Input
-            label={'장비 사진'}
-            type={'file'}
-            onChange={e => setImage(e.target.files[0])}
-          />
-          <p>이미지가 없으면 기본 이미지가 표시됩니다.</p>
+
+          <Message>
+            <Message.Header>장소 이미지</Message.Header>
+            <p>
+              장소 이미지는 장소 생성 후에 설정 할 수 있습니다.
+              장소의 이미지가 없으면 기본 이미지가 표시됩니다.
+            </p>
+          </Message>
+
           <Modal.Actions>
             <Form.Group>
               <Form.Button
