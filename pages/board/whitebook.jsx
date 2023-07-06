9778e8f84061eed99dc3e46084e72a28c96d1279
bluehorn07 <bluehornblues@gmail.com>
2023-07-07 00:26:31 +0900

    apply SSR to whitebook

--- pages/board/whitebook.jsx
+++ pages/board/whitebook.jsx
@@ -1,24 +1,11 @@
-import { useEffect, useState } from 'react'
 import { Button } from 'semantic-ui-react'
+
 import BoardLayout from '@/components/board/board.layout'
 import WhitebookCreateModal from '@/components/board/whitebook.create.modal'
 import WhitebookTable from '@/components/board/whitebook.table'
 import { PoPoAxios } from '@/utils/axios.instance';
 
-const WhitebookPage = () => {
-  const [whitebooks, setWhitebooks] = useState([])
-
-  useEffect(() => {
-    PoPoAxios.get(
-      '/whitebook/with-login?orderBy=click_count',
-      { withCredentials: true }).
-      then((res) => setWhitebooks(res.data)).
-      catch((err) => {
-        alert('생활백서 목록을 불러오는데 실패했습니다.')
-        console.log(err)
-      })
-  }, [])
-
+const WhitebookPage = ({ whitebookList }) => {
   return (
     <BoardLayout>
       <h3>생활백서</h3>
@@ -30,11 +17,21 @@ const WhitebookPage = () => {
       <p>생활백서는 조회순으로 정렬되어 표시됩니다!</p>
       <div>
         <WhitebookTable
-          whitebooks={whitebooks}
+          whitebooks={whitebookList}
         />
       </div>
     </BoardLayout>
   )
 }
 
-export default WhitebookPage
+export default WhitebookPage;
+
+export async function getServerSideProps() {
+  const res = await PoPoAxios.get(
+    'whitebook/with-login?orderBy=click_count',
+    { withCredentials: true }
+  );
+  const whitebookList = res.data;
+
+  return { props: { whitebookList } };
+}
