687af0d8a2b0aa7f332dacba12f68b759783ea3a
bluehorn07 <bluehornblues@gmail.com>
2023-07-07 00:08:02 +0900

    create modal -> create page

--- pages/equipment/index.jsx
+++ pages/equipment/index.jsx
@@ -1,9 +1,8 @@
+import Link from "next/link";
 import { Button } from 'semantic-ui-react'
 
 import ReservationLayout from '@/components/reservation/reservation.layout'
 import EquipmentTable from '@/components/equipment/equipment.table'
-import EquipmentCreateModal
-  from '@/components/equipment/equipment.create.modal'
 import { PoPoAxios } from "@/utils/axios.instance";
 
 const EquipmentPage = ({ equipmentList }) => {
@@ -11,9 +10,9 @@ const EquipmentPage = ({ equipmentList }) => {
     <ReservationLayout>
       <h3>장비 목록</h3>
       <div style={{marginBottom: "1rem"}}>
-        <EquipmentCreateModal
-          trigger={<Button>장비 생성</Button>}
-        />
+        <Link href={'/equipment/create'}>
+          <Button>장비 생성</Button>
+        </Link>
       </div>
       <p>
         장비는 마지막 수정일 순서로 정렬되어 표시됩니다!
