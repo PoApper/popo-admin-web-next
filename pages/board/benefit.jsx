b87a4aff8c264c90731cc7ab7c4063385df4e6c3
HyoJeong Yun <hodori@postech.ac.kr>
2023-08-13 15:04:07 +0900

    implt benefit page

---
+++ pages/board/benefit.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+
+import { Divider } from 'semantic-ui-react'
+import styled from 'styled-components'
+import BoardLayout from '@/components/board/board.layout'
+import AffiliateCards from '@/components/board/affiliate.cards'
+import DiscountOfferCards from '@/components/board/discount.cards'
+
+import { affiliates, discountOffers } from './affiliate.data'; // Import the variable from the data.js file
+
+
+const BenefitPage = () => {
+  return (
+    <BoardLayout>
+      <h3>총학생회 제휴 업체 소개</h3>
+        <AffiliateCards
+          affiliates={affiliates}
+        />
+      <AffiliateDivider/>
+      <h3>총학생회 할인 업체 소개</h3>
+        <DiscountOfferCards
+          discountOffers={discountOffers}
+        />
+    </BoardLayout>
+  )
+}
+
+const AffiliateDivider = styled(Divider)`  
+  margin: 20px 0px 0px 0px;
+  padding: 20px 0px 0px 0px;
+`
+
+export default BenefitPage
