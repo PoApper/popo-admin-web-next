d74a08c4431241961627b25a5da0ebb6155f1f48
HyoJeong Yun <hodori@postech.ac.kr>
2023-09-11 19:42:20 +0900

    add new setting variable

--- pages/board/setting.jsx
+++ pages/board/setting.jsx
@@ -7,12 +7,16 @@ import { PoPoAxios, PopoCdnAxios } from '@/utils/axios.instance';
 const SettingPage = () => {
   const [popoCRMEmail, setPOPOCRMEmail] = useState('');
   const [dongyeonBank, setDongyeonBank] = useState('');
+  const [dongyeonServiceTime, setDongyeonServiceTime] = useState('');
+  const [donyeonContact, setDongyeonContact] = useState('');
 
   useEffect(() => {
     PopoCdnAxios.get('/popo-setting.json')
       .then((res) => {
         setPOPOCRMEmail(res.data.popo_crm_email);
         setDongyeonBank(res.data.dongyeon_bank);
+        setDongyeonServiceTime(res.data.dongyeon_service_time);
+        setDongyeonContact(res.data.dongyeon_contact);
       }).catch((err) => {
         const errMsg = err.response.data.message;
         alert(`설정값을 불러오는데 실패했습니다.\n${errMsg}`);
@@ -23,6 +27,8 @@ const SettingPage = () => {
     PoPoAxios.post('/setting', {
       popo_crm_email: popoCRMEmail,
       dongyeon_bank: dongyeonBank,
+      dongyeon_service_time: dongyeonServiceTime,
+      dongyeon_contact: donyeonContact,
     }, {withCredentials: true})
       .then(() => alert('설정값을 저장했습니다!'))
       .catch((err) => {
@@ -48,6 +54,16 @@ const SettingPage = () => {
           label={'동아리 연합회 계좌 (장비 예약비)'}
           value={dongyeonBank}
           onChange={e => setDongyeonBank(e.target.value)}
+        />
+        <Form.Input
+          label={'동아리 연합회 대여시간'}
+          value={dongyeonBank}
+          onChange={e => setDongyeonServiceTime(e.target.value)}
+        />
+        <Form.Input
+          label={'동아리 연합회 문의 번호'}
+          value={dongyeonBank}
+          onChange={e => setDongyeonContact(e.target.value)}
         />
 
         <Form.Button 
