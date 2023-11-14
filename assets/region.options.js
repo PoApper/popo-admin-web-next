442dbd2020cf17f8c03e1f8083a2011247522b22
HyoJeong Yun <hodori@postech.ac.kr>
2023-11-14 11:04:52 +0100

    [fix] inconsistency

--- assets/region.options.js
+++ assets/region.options.js
@@ -2,6 +2,6 @@ export const RegionOptions = [
   { key: "STUDENT_HALL", text: "학생 회관", value: "STUDENT_HALL" },
   { key: "JIGOK_CENTER", text: "지곡 회관", value: "JIGOK_CENTER" },
   { key: "COMMUNITY_CENTER", text: "커뮤니티 센터", value: "COMMUNITY_CENTER" },
-  { key: "RC", text: "RC", value: "RC" },
+  { key: "RESIDENTIAL_COLLEGE", text: "RC", value: "RESIDENTIAL_COLLEGE" },
   { key: "OTHERS", text: "기타", value: "OTHERS" },
 ];
