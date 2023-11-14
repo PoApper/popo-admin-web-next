6508331403933995429a2a6ef5e2bb7117c71fc6
HyoJeong Yun <hodori@postech.ac.kr>
2023-11-14 11:36:43 +0100

    Update reservation regions (#59)

--- assets/region.options.js
+++ assets/region.options.js
@@ -2,5 +2,6 @@ export const RegionOptions = [
   { key: "STUDENT_HALL", text: "학생 회관", value: "STUDENT_HALL" },
   { key: "JIGOK_CENTER", text: "지곡 회관", value: "JIGOK_CENTER" },
   { key: "COMMUNITY_CENTER", text: "커뮤니티 센터", value: "COMMUNITY_CENTER" },
-  { key: "OTHERS", text: "생활관 외", value: "OTHERS" },
+  { key: "RESIDENTIAL_COLLEGE", text: "RC", value: "RESIDENTIAL_COLLEGE" },
+  { key: "OTHERS", text: "기타", value: "OTHERS" },
 ];
