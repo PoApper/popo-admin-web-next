c005d20bc9d74687099af2d87d81c69b994cb98b
bluehorn07 <bluehornblues@gmail.com>
2024-01-12 00:29:15 +0900

    equip reserv table polish

--- components/equipment/equipment.reservation.table.jsx
+++ components/equipment/equipment.reservation.table.jsx
@@ -4,72 +4,67 @@ import moment from 'moment'
 import EquipmentReservationConfirmModal
   from './equipment.reservation.confirm.modal'
 
-const EquipmentReservationTable = (props) => {
-  const reservations = props.reservations
-  const start_idx = props.startIdx
-
+const EquipmentReservationTable = ({ reservations, startIdx }) => {
   return (
-    <>
-      <Table
-        celled selectable
-        textAlign={'center'}>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell width={1}>idx.</Table.HeaderCell>
-            <Table.HeaderCell width={3}>장비 목록</Table.HeaderCell>
-            <Table.HeaderCell width={2}>사용자</Table.HeaderCell>
-            <Table.HeaderCell>예약 제목</Table.HeaderCell>
-            <Table.HeaderCell width={4}>예약 기간</Table.HeaderCell>
-            <Table.HeaderCell width={2}>상태</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {
-            reservations.map((reservation, idx) =>
-              <EquipmentReservationConfirmModal
-                key={reservation.uuid}
-                reservation={reservation}
-                trigger={
-                  <Table.Row key={reservation.uuid}>
-                    <Table.Cell>{start_idx + idx + 1}</Table.Cell>
-                    <Table.Cell>
-                      {
-                        reservation.equipments.map(equipment => {
-                          return (
-                            <Label
-                              size={"tiny"}
-                              key={equipment.uuid}
-                              style={{margin: "2px"}}
-                            >
-                              {equipment.name}
-                            </Label>
-                          )
-                        })
-                      }
-                    </Table.Cell>
-                    <Table.Cell>{reservation.booker.name}</Table.Cell>
-                    <Table.Cell>{reservation.title}</Table.Cell>
-                    <Table.Cell>
-                      <b>
-                        {moment(reservation.date, 'YYYYMMDD').
-                          format('YYYY년 MM월 DD일')}
-                        <br/>
-                        {moment(reservation.start_time, 'HHmm').
-                          format('HH:mm')}
-                        &nbsp;~&nbsp;
-                        {moment(reservation.end_time, 'HHmm').
-                          format('HH:mm')}
-                      </b>
-                    </Table.Cell>
-                    <Table.Cell>{reservation.status}</Table.Cell>
-                  </Table.Row>
-                }
-              />,
-            )
-          }
-        </Table.Body>
-      </Table>
-    </>
+    <Table
+      celled selectable
+      textAlign={'center'}>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell width={1}>idx.</Table.HeaderCell>
+          <Table.HeaderCell width={3}>장비 목록</Table.HeaderCell>
+          <Table.HeaderCell width={2}>사용자</Table.HeaderCell>
+          <Table.HeaderCell>예약 제목</Table.HeaderCell>
+          <Table.HeaderCell width={4}>예약 기간</Table.HeaderCell>
+          <Table.HeaderCell width={2}>상태</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {
+          reservations.map((reservation, idx) =>
+            <EquipmentReservationConfirmModal
+              key={reservation.uuid}
+              reservation={reservation}
+              trigger={
+                <Table.Row key={reservation.uuid}>
+                  <Table.Cell>{startIdx + idx + 1}</Table.Cell>
+                  <Table.Cell>
+                    {
+                      reservation.equipments.map(equipment => {
+                        return (
+                          <Label
+                            size={"tiny"}
+                            key={equipment.uuid}
+                            style={{margin: "2px"}}
+                          >
+                            {equipment.name}
+                          </Label>
+                        )
+                      })
+                    }
+                  </Table.Cell>
+                  <Table.Cell>{reservation.booker.name}</Table.Cell>
+                  <Table.Cell>{reservation.title}</Table.Cell>
+                  <Table.Cell>
+                    <b>
+                      {moment(reservation.date, 'YYYYMMDD').
+                        format('YYYY년 MM월 DD일')}
+                      <br/>
+                      {moment(reservation.start_time, 'HHmm').
+                        format('HH:mm')}
+                      &nbsp;~&nbsp;
+                      {moment(reservation.end_time, 'HHmm').
+                        format('HH:mm')}
+                    </b>
+                  </Table.Cell>
+                  <Table.Cell>{reservation.status}</Table.Cell>
+                </Table.Row>
+              }
+            />,
+          )
+        }
+      </Table.Body>
+    </Table>
   )
 }
 
