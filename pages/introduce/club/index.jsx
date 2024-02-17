55d680f95a90067155d1832092743bdd7ffba5d7
bluehorn07 <bluehornblues@gmail.com>
2024-02-17 16:43:24 +0900

    select by club type

--- pages/introduce/club/index.jsx
+++ pages/introduce/club/index.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import Link from "next/link";
-import { Button } from 'semantic-ui-react'
+import { Button, Select } from 'semantic-ui-react'
 
 import IntroduceLayout from '@/components/introduce/introduce.layout'
 import ClubTable from '@/components/introduce/club.table'
 import { PoPoAxios } from '@/utils/axios.instance';
+import { ClubTypeOptions } from '@/assets/club.type.options';
+
+const SelectClubTypeOptions = [
+  { key: 'ALL', value: 'ALL', text: '전체' },
+  ...ClubTypeOptions,
+]
 
 const ClubIntroducePage = ({ clubList }) => {
+  const [selectedClubType, setSelectedClubType] = useState('ALL');
+
+  const filteredClubList = useMemo(() => {
+    if (selectedClubType === 'ALL') return clubList;
+    return clubList.filter(club => club.clubType === selectedClubType);
+
+  }, [selectedClubType, clubList])
+
+
   return (
     <IntroduceLayout>
       <h3>동아리 소개글</h3>
-      <div style={{marginBottom: "1rem"}}>
-        <Link href={'/introduce/club/create'}>
-          <Button>동아리 생성</Button>
-        </Link>
+      <div style={{ marginBottom: '1rem', display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{marginBottom: "1rem"}}>
+          <Link href={'/introduce/club/create'}>
+            <Button>동아리 생성</Button>
+          </Link>
+        </div>
+        <div>
+          <Select
+            value={selectedClubType}
+            options={SelectClubTypeOptions}
+            onChange={(e, { value }) => setSelectedClubType(value)}
+          />
+        </div>
       </div>
       <div>
         <ClubTable
-          clubs={clubList}
+          clubs={filteredClubList}
         />
       </div>
     </IntroduceLayout>
