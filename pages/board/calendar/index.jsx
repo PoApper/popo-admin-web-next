be28b7eb115d6debc6f3698204c7259cfe389c29
Seokyun Ha <bluehornblues@gmail.com>
2024-06-23 11:34:23 +0900

    Display Equip Reservation Statistics (#80)

--- pages/board/calendar/index.jsx
+++ pages/board/calendar/index.jsx
@@ -9,16 +9,31 @@ const AnnouncementPage = ({ calendarList }) => {
   return (
     <BoardLayout>
       <h3>학사 일정</h3>
-      <div style={{ marginBottom: '1rem' }}>
+      <div
+        style={{
+          marginBottom: '1rem',
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
         <Link href={'/board/calendar/create'}>
           <Button>학사일정 추가</Button>
         </Link>
+
+        <Button
+          href={
+            'https://www.postech.ac.kr/academics/academic-info/academic-calendar/'
+          }
+          style={{ backgroundColor: 'rgb(200, 1, 80)', color: 'white' }}
+        >
+          공식 학사일정 확인하기
+        </Button>
       </div>
 
       <Message>학사일정은 시작 일자로 정렬되어 표시됩니다!</Message>
       <Message>
-        만약 이틀 이상 진행되는 일정이라면 시작/종료 일정으로 분리해주세요.{' '}
-        <br />
+        만약 이틀 이상 진행되는 일정이라면 <b>시작/종료</b> 일정으로
+        분리해주세요. <br />
         (ex. 2학기 수강 신청 (05.20 ~ 05.28) → 2학기 수강신청 수강신청
         시작(05.20) / 2학기 수강신청 마감(05.28))
       </Message>
